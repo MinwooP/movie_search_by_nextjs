a030d8af3ab96af7600299babfb59466aa2dbe83
박민우 <alsd20177@gmail.com>
2023-10-11 18:48:07 +0900

    feat: queryClient suspense 사용 설정

--- src/components/Provider/index.tsx
+++ src/components/Provider/index.tsx
@@ -1,12 +1,19 @@
-'use client'
+"use client";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import React from 'react'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
 
-export default function Providers({ children }: {children: React.ReactNode}) {
-  const [queryClient] = React.useState(() => new QueryClient())
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            suspense: true,
+          },
+        },
+      })
+  );
 
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  )
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
